df801e4bedb1200c0aa62638ec860dbf47d3ba5d
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-11-12 21:56:24 -0300

    Game engine is done
    now I will learn you to change CSS with javascript and use it in my interface. But the rest is made !! THANKSSS

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,36 +2,72 @@
 function numberSelectionToString(choiceValue){
   switch (choiceValue) {
       case 0:
-        return 'rock'
+        return 'Rock'
       case 1:
-        return 'paper'
+        return 'Paper'
       case 2:
-        return 'scissors'
+        return 'Scissors'
     }
 }
 
+function gameResult(playerValue, machineValue){
+  let gameResultTag = document.getElementById("gameResult");
+  if(playerValue === machineValue){
+    return gameResultTag.textContent = "Empate";
+  }else{
+    winCheck(playerValue, machineValue);
+  }
+}
+
+function winCheck(playerValue, machineValue){
+  let gameResultTag = document.getElementById("gameResult");
+  if(playerValue == 0 && machineValue == 1){
+    machineWinCount();
+    return gameResultTag.textContent = "Machine win";
+  }else if(playerValue == 1 && machineValue == 2){
+    machineWinCount();
+    return gameResultTag.textContent = "Machine win";
+  }else if(playerValue == 2 && machineValue == 0){
+    machineWinCount();
+    return gameResultTag.textContent = "Machine win";
+  }else{
+    playerWinCount();
+    return gameResultTag.textContent = "You win!";
+  }
+}
+
+function machineWinCount(){
+  let machinePointsTag = document.getElementById("machinePoints");
+  if(parseInt(machinePointsTag.textContent) != 5){
+    machinePointsTag.textContent = (parseInt(machinePointsTag.textContent)) + 1;
+  }else{
+    machinePointsTag.textContent = "MACHINE WIN";
+  }
+}
+
+function playerWinCount(){
+  let playerPointsTag = document.getElementById("playerPoints");
+  if(parseInt(playerPointsTag.textContent) != 5){
+    playerPointsTag.textContent = (parseInt(playerPointsTag.textContent)) + 1;
+  }else{
+    playerPointsTag.textContent = "YOU WIN";
+  }
+}
+
+function choiceDisplay(playerSelection, computerSelection){
+  let playerActionText = document.createElement("li");
+  let machineActionText = document.createElement("li");
+  let playerChoiceTag = document.getElementById("playerChoice");
+  let machineChoiceTag = document.getElementById("machineChoice");
+  playerActionText.textContent = numberSelectionToString(playerSelection);
+  machineActionText.textContent = numberSelectionToString(computerSelection);
+  playerChoiceTag.appendChild(playerActionText);
+  machineChoiceTag.appendChild(machineActionText);
+}
+
 function runGame(playerSelection){
-    //random computer choice
-    const computerSelection = parseInt(Math.random() * 3)
-    let playerPointsTag = document.getElementById("playerPoints");
-    let machinePointsTag = document.getElementById("machinePoints");
-    let playerChoiceTag = document.getElementById("playerChoice");
-    let machineChoiceTag = document.getElementById("machineChoice");
-
-    console.log(playerPoints);
-    console.log(machinePoints);
-    console.log(playerChoice);
-    console.log(machineChoice);
-    
-    let playerAction = "aaa"
-    a = document.appendChild(playerAction);
-
-    a.textContent += "playerAction";
-    //empate e acaba
-    if(playerSelection === computerSelection){
-        return;
-    }
-    
-
-   return;
+  const computerSelection = parseInt(Math.random() * 3);
+  gameResult(playerSelection, computerSelection);
+  choiceDisplay(playerSelection, computerSelection);
+  return;
 }
